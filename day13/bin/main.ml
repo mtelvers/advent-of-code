a73aca32f9dbbe3547a54e552b92a3e294c10be4
Mark Elvers <mark.elvers@tunbury.org>
2022-12-15 19:59:04 +0000

    Day 13 complete

--- day13/bin/main.ml
+++ day13/bin/main.ml
@@ -1,7 +1,6 @@
 type 'a node = One of 'a | Many of 'a node list
 
 let to_node_list txt =
-            let () = Printf.printf "\ntxt: %s\n" txt in
   let rec parse (lst : Str.split_result list) =
     match lst with
     | Text t :: tl -> One (int_of_string t) :: parse tl
@@ -18,27 +17,13 @@ let to_node_list txt =
           | [] -> (left, [])
         in
         match parselist 1 [] tl with
-        | sub, rest ->
-            let print x =
-                  match (x : Str.split_result) with
-                  | Delim d -> Printf.printf " %s " d
-                  | Text t -> Printf.printf " %s " t in
-            let () = Printf.printf "tl: " in
-            let () = List.iter (print) tl in 
-            let () = Printf.printf "\n" in
-            let () = Printf.printf "sub: " in
-            let () = List.iter (print) sub in
-            let () = Printf.printf "\n" in
-            let () = Printf.printf "rest: " in
-            let () = List.iter (print) rest in
-            let () = Printf.printf "\n\n" in
-
-            Many (parse sub) :: parse rest)
+        | sub, rest -> Many (parse sub) :: parse rest)
     | Delim "]" :: _ -> assert false
     | _ -> []
   in
   parse (Str.full_split (Str.regexp "[],[]") txt)
 
+(*
 let from_node_list lst =
   let rec loop acc lst =
     List.fold_left
@@ -49,35 +34,26 @@ let from_node_list lst =
       acc lst
   in
   loop "" lst
+*)
 
 let c le ri =
   let rec loop le ri =
     match (le, ri) with
     | One lh :: lt, One rh :: rt ->
-        let () = Printf.printf "O , O " in
-        let () = Printf.printf "%i , %i\n" lh rh in
         let r = compare lh rh in
         if r = 0 then loop lt rt else r
     | Many lh :: lt, Many rh :: rt ->
-        let () = Printf.printf "M , M\n" in
         let r = loop lh rh in
         if r = 0 then loop lt rt else r
     | One lh :: lt, Many rh :: rt ->
-        let () = Printf.printf "O , M " in
-        let () = Printf.printf "%i\n" lh in
         let r = loop [ One lh ] rh in
         if r = 0 then loop lt rt else r
     | Many lh :: lt, One rh :: rt ->
-        let () = Printf.printf "M , O " in
-        let () = Printf.printf "%i\n" rh in
         let r = loop lh [ One rh ] in
         if r = 0 then loop lt rt else r
-    | [], _ ->
-        let () = Printf.printf "*true*\n" in
-        -1
-    | _, [] ->
-        let () = Printf.printf "*false*\n" in
-        1
+    | [], [] -> 0
+    | [], _ -> -1
+    | _, [] -> 1
   in
   loop le ri
 
@@ -89,21 +65,7 @@ let comparisons =
       let left_nodes = to_node_list left in
       let right = input_line input in
       let right_nodes = to_node_list right in
-      let () =
-        Printf.printf "\nPair %i\n left: %s\n left: %s\nright: %s\nright: %s\n"
-          pair left
-          (from_node_list left_nodes)
-          right
-          (from_node_list right_nodes)
-      in
-      let result =
-        if c left_nodes right_nodes < 0 then
-          let () = Printf.printf "true\n" in
-          pair
-        else
-          let () = Printf.printf "false\n" in
-          0
-      in
+      let result = if c left_nodes right_nodes < 0 then pair else 0 in
       let _ = input_line input in
       loop input (pair + 1) (acc + result)
     with End_of_file -> acc
@@ -111,3 +73,29 @@ let comparisons =
   loop ic 1 0
 
 let () = Printf.printf "Total %i\n" comparisons
+
+let comparisons =
+  let ic = open_in "input" in
+  let rec loop input =
+    try
+      let line = input_line input in
+      if String.length line = 0 then loop input
+      else to_node_list line :: loop input
+    with End_of_file -> []
+  in
+  loop ic
+
+let () =
+  let dividers =
+    List.fold_left
+      (fun acc txt -> to_node_list txt :: acc)
+      [] [ "[[2]]"; "[[6]]" ]
+  in
+  let sorted = List.sort c (comparisons @ dividers) in
+  let _, total =
+    List.fold_left
+      (fun (i, t) el ->
+        if List.mem el dividers then (i + 1, t * i) else (i + 1, t))
+      (1, 1) sorted
+  in
+  Printf.printf "Total %i\n" total
