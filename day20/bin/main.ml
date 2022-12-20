147f9848a6dcfef482436354df6d996deea3b6f3
Mark Elvers <mark.elvers@tunbury.org>
2022-12-20 20:34:51 +0000

    Failed array approach

--- day20/bin/main.ml
+++ day20/bin/main.ml
@@ -13,73 +13,54 @@ let input_list =
   in
   loop ic
 
-let () = List.iter (Printf.printf "%i,") input_list
-let () = Printf.printf "\n"
+let numbers = Array.of_list input_list
+let rec find a x n = if a.(n) = x then n else find a x (n + 1)
 
-exception Failure of string
+let swap a i1 i2 =
+  let n1 = a.(i1) in
+  let n2 = a.(i2) in
+  let () = Array.set a i2 n1 in
+  Array.set a i1 n2
 
-let rec find x lst =
-  match lst with
-  | [] -> raise (Failure "Not Found")
-  | h :: t -> if x = h then 0 else 1 + find x t
+let () =
+  List.iteri
+    (fun i number ->
+   let () = Printf.printf "%i %i\n" i number in
+   let () = flush stdout in
+      let () =
+        let () = Array.iter (Printf.printf "%i,") numbers in
+        Printf.printf "\n"
+      in
+      let len = Array.length numbers in
+      let rec loop num =
+        if num = 0 then
+          ()
+         else (
+            let index = find numbers number 0 in
+            if index + 1 = len then
+                let n1 = numbers.(len - 1) in
+                for i = len - 1 downto 1 do
+                  swap numbers i (i - 1)
+                done;
+                let () = Array.set numbers 0 n1 in
+                loop num
+            else
+                let () = swap numbers index (index + 1) in
+                loop (num - 1))
+      in
+      loop (if number < 0 then (number mod len) + len - 1 else number mod len))
+    input_list
 
-let rec split_at n acc l =
-  if n = 0 then (List.rev acc, l)
-  else
-    match l with
-    | [] -> (List.rev acc, [])
-    | h :: t -> split_at (n - 1) (h :: acc) t
-
-let print_list str lst =
-  let () = Printf.printf "%s: " str in
-  let () = List.iter (Printf.printf "%i,") lst in
+let () =
+  let () = Array.iter (Printf.printf "%i,") numbers in
   Printf.printf "\n"
 
-let output_list =
-  List.fold_left
-    (fun acc number ->
-      let index = find number acc in
-      let len = List.length acc in
-      let num = number mod len in
-      let num = num + number / len in
-      let dir, delta =
-        if index + num >= len then (`Left, index + num - len + 1)
-        else if index + num < 0 then (`Right, len + num - 1)
-        else
-          ((if num = 0 then `Fixed else if num > 0 then `Right else `Left), index + num)
-      in
-      let () = Printf.printf "%i %i %i\n" num index delta in
-      match dir with
-      | `Fixed -> acc
-      | `Left ->
-          let left, right = split_at index [] acc in
-          let () = print_list "left" left in
-          let () = print_list "right" right in
-          let left_left, left_right = split_at delta [] left in
-          let () = print_list "left_left" left_left in
-          let () = print_list "left_right" left_right in
-          let result = left_left @ [ number ] @ left_right @ List.tl right in
-          let () = print_list "result" result in
-          result
-      | `Right ->
-          let left, right = split_at index [] acc in
-          let () = print_list "left" left in
-          let () = print_list "right" right in
-          let right_left, right_right = split_at delta [] (List.tl right) in
-          let () = print_list "right_left" right_left in
-          let () = print_list "right_right" right_right in
-          let result = left @ right_left @ [ number ] @ right_right in
-          let () = print_list "result" result in
-          result)
-    input_list input_list
-
-let () = print_list "result" output_list
-let index_of_zero = find 0 output_list
+let index_of_zero = find numbers 0 0
 let () = Printf.printf "%i\n" index_of_zero
 let () = Printf.printf "Sum of 1000th (%i) + 2000th (%i) + 3000th (%i) = %i\n" 
-(List.nth output_list ((1000 + index_of_zero) mod (List.length output_list)))
-(List.nth output_list ((2000 + index_of_zero) mod (List.length output_list)))
-(List.nth output_list ((3000 + index_of_zero) mod (List.length output_list)))
-((List.nth output_list ((1000 + index_of_zero) mod (List.length output_list))) +
-(List.nth output_list ((2000 + index_of_zero) mod (List.length output_list))) +
-(List.nth output_list ((3000 + index_of_zero) mod (List.length output_list))))
+(Array.get numbers ((10 + index_of_zero) mod (Array.length numbers)))
+(Array.get numbers ((20 + index_of_zero) mod (Array.length numbers)))
+(Array.get numbers ((30 + index_of_zero) mod (Array.length numbers)))
+((Array.get numbers ((10 + index_of_zero) mod (Array.length numbers))) +
+(Array.get numbers ((20 + index_of_zero) mod (Array.length numbers))) +
+(Array.get numbers ((30 + index_of_zero) mod (Array.length numbers))))
